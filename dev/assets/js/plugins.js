8d591a6fce741af82f2208a3d8939273cf48840f
Gustavo Esquinca <g.esq.ca@gmail.com>
2014-03-27 16:13:28 -0600

    An f-load of sidebar things…
    +svg icons
    +svg spirte
    +sidebar styles
    +grunt
    +svg store

--- dev/assets/js/plugins.js
+++ dev/assets/js/plugins.js
@@ -1,3 +1,8 @@
+// @codekit-prepend: '_plugins/jquery.transit.js';
+
+// @codekit-prepend: '_plugins/jquery.history.js';
+// @codekit-prepend: '_plugins/ajaxify-html5.js';
+
+// @codekit-prepend: '_plugins/jquery.scroll_perf.js';
 // @codekit-prepend: '_plugins/headroom.js';
 // @codekit-prepend: '_plugins/jQuery.headroom.js';
-// @codekit-prepend: '_plugins/jquery.scroll_perf.js';
--- dev/assets/js/scripts.js
+++ dev/assets/js/scripts.js
@@ -5,6 +5,8 @@
   // R works as alias for Response in here - R.band(320) instead of Response.band(320)
   // window and document minify down to single letter vars
 
+  var $body = $('body');
+
   $('#nav--global').headroom({
     'tolerance': 12,
     'offset': 0,
@@ -12,7 +14,7 @@
 
   $(window).on('scroll_perf', function (event) {
     // body...
-    console.log(event.scrollTop);
+    // console.log(event.scrollTop);
 
     if (event.scrollTop > 0) {
       $('.nav--local').find('.ui--dropdown').css('background', 'none');
@@ -27,7 +29,51 @@
 // find position
 // apply class when window reaches the position
 
-$('');
+
+// TODO:
+var sideNavTooltip = function () {
+  // body...
+  var nav       = $('#nav--local');
+  var navHeight = nav.height();
+  var tooltip   = nav.find('.nav--tooltip');
+  var tooltipOffset   = tooltip.outerHeight() / 2;
+
+  var currentPage       = $body.data('page');
+  var navLinks = nav.find('li>a');
+
+
+  $.fn.clickHandler = function () {
+    // e.preventDefault();
+    var $this = $(this);
+    var elem = $this.parent();
+    var coordinates = elem.position().top + (elem.height() / 2) - tooltipOffset;
+
+    tooltip.transition({
+      'top': coordinates
+    });
+
+    navLinks.removeClass('active');
+    $this.addClass('active');
+
+    return $this;
+  };
+
+  navLinks.filter(function() {
+    return $(this).data('page') === currentPage;
+  }).clickHandler();
+
+  navLinks.on('click', function(e) {
+    // e.preventDefault();
+    $(this).clickHandler();
+  });
+
+  // console.log(currentPage);
+  console.log(navLinks.eq(1));
+
+};
+// init
+sideNavTooltip();
+
 
 
 }(this, this.document, this.jQuery)); // in global scope, this === window
