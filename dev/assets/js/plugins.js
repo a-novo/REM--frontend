b6af0fb9e8207a55b6e721a752321deca77c8d64
Gustavo Esquinca <g.esq.ca@gmail.com>
2014-04-04 09:47:15 -0600

    removed index and ajaxify scripts.

--- dev/assets/js/plugins.js
+++ dev/assets/js/plugins.js
@@ -1,8 +1,5 @@
 // @codekit-prepend: '_plugins/jquery.transit.js';
 
-// @codekit-prepend: '_plugins/jquery.history.js';
-// @codekit-prepend: '_plugins/ajaxify-html5.js';
-
 // @codekit-prepend: '_plugins/jquery.scroll_perf.js';
 // @codekit-prepend: '_plugins/headroom.js';
 // @codekit-prepend: '_plugins/jQuery.headroom.js';
